1a863e879c7a6e2b461fc5a6e1a6ac00757da0f3
Swastik Sharma <swastiksharma88@gmail.com>
2024-05-10 02:41:06 +0530

    Added more files

---
+++ server/db/database.py
@@ -0,0 +1,50 @@
+class Database:
+    def __init__(self,user,password,host,database,port="5432"):
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.database = database
+        self._cursor = None
+
+        self._connection_pool = None
+        
+    async def connect(self):
+        if not self._connection_pool:
+            try:
+                self._connection_pool = await asyncpg.create_pool(
+                    min_size=1,
+                    max_size=20,
+                    command_timeout=60,
+                    host=self.host,
+                    port=self.port,
+                    user=self.user,
+                    password=self.password,
+                    database=self.database,
+                    ssl="require"
+                )
+                logger.info("Database pool connectionn opened")
+
+            except Exception as e:
+                logger.exception(e)
+
+    async def fetch_rows(self, query: str,*args):
+        if not self._connection_pool:
+            await self.connect()
+        else:
+            con = await self._connection_pool.acquire()
+            try:
+                result = await con.fetch(query,*args)
+                return result
+            except Exception as e:
+                logger.exception(e)
+            finally:
+                await self._connection_pool.release(con)
+
+    async def close(self):
+        if not self._connection_pool:
+            try:
+                await self._connection_pool.close()
+                logger.info("Database pool connection closed")
+            except Exception as e:
+                logger.exception(e)
